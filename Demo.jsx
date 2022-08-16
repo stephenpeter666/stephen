3b09f6be39c7c7a0f7cfec8e7f64c2fca1ee7fb8
stephenpeter666 <111300092+stephenpeter666@users.noreply.github.com>
2022-08-15 18:49:27 -0700

    first commit

---
+++ Demo.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Demo = () => {
+    let p = 51;
+    const w = 60;
+    for(i = 1; i => 10; i++; ) {
+
+    }
+
+    if(w > 10) {
+        return (
+            <p>w is greater than 10</p>
+        )
+    } else {
+        <p>w is less than 10</p>
+    }
+  return (
+    <section>
+      <main className='p'>
+        {w > 10 ? 'w is greater than 10' : 'w is less than 10'}
+      </main>
+    </section>
+    
+  )
+}
+
+export default Demo
